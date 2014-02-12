9ac705eb577c4d3d36310cbc3ad40a823d6fc054
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-12 17:09:13 -0600

    billing overview route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('billingApp', ['ngRoute', 'ngResource'])
+angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui.tpls', 'encore.ui.rxForm', 'encore.ui.rxPaginate'])
     .config(function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/login', {
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
+            })
+            .when('/billing/overview', {
+                templateUrl: '/views/billing/overview.html',
+                controller: 'OverviewCtrl'
             })
             .otherwise({
                 redirectTo: '/login'
