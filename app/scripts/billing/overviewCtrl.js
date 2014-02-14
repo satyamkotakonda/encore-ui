19c5ff52421e738f9021d399b0140e464fc80d8f
Chris Cantu <christopher.cantu@gmail.com>
2014-02-13 21:08:08 -0700

    Fixed dropdown error I caused =)

--- app/scripts/billing/overviewCtrl.js
+++ app/scripts/billing/overviewCtrl.js
@@ -8,18 +8,29 @@ angular.module('billingApp')
         $scope.account = Account.get({ id: $routeParams.accountNumber });
         $scope.transactions = Transaction.list({ id: $routeParams.accountNumber });
 
-        var anyValue = {
-            value: undefined,
-            label: 'Any'
-        };
-        // This is most likely done differently, from an API call maybe? similar concept though.
-        $scope.transactionTypes = [anyValue, 'Payment', 'Invoice', 'Reversal', 'Adjustment'];
-        $scope.transactionStatus = [anyValue, 'Paid', 'Settled', 'Unpaid'];
+        var anyValue = { value: undefined, label: 'Any' },
+            itemToOption = function (val) {
+                if (val.label) {
+                    return val;
+                } else if (_.isString(val)) {
+                    return { value: val, label: val };
+                }
+
+                return { value: val[0], label: val[1] };
+            };
+        
+        $scope.transactionTypes = [anyValue, 'Payment', 'Invoice', 'Reversal',
+            'Adjustment'].map(itemToOption);
+        $scope.transactionStatus = [anyValue, 'Paid', 'Settled', 'Unpaid']
+            .map(itemToOption);
         $scope.transactionDate = [anyValue, [ -1, 'Current Period'],
-            [ -2, 'Previous Statement'], [ -4, 'Last 3 Statements']];
+            [ -2, 'Previous Statement'], [ -4, 'Last 3 Statements']]
+            .map(itemToOption);
 
         $scope.sort = {
             field: 'date',
             reverse: true
         };
+
+        console.log($scope.transactionTypes);
     });
