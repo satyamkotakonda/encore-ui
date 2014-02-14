0598b57d1cc784dd402c5562093ac083a89c6ae8
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-14 09:58:28 -0600

    jscsified, changed variable names

--- app/scripts/billing/overviewCtrl.spec.js
+++ app/scripts/billing/overviewCtrl.spec.js
@@ -1,13 +1,20 @@
-
+/* jshint node: true */
 describe('Billing: OverviewCtrl', function () {
-    var scope, ctrl;
-    beforeEach( function () {
-        module('billingApp');
-        inject(function ($controller, $rootScope) {
-            scope = $rootScope.$new();
+    var scope, ctrl, PageTrackingObject;
 
+    var testAccountNumber = '1020121';
+
+    beforeEach(function () {
+        module('billingApp');
+        inject(function ($controller, $rootScope, PageTracking) {
+            scope = $rootScope.$new();
+            PageTrackingObject = PageTracking.createInstance().constructor;
+            
             ctrl = $controller('OverviewCtrl',{
-                $scope: scope
+                $scope: scope,
+                $routeParams: {
+                    accountNumber: testAccountNumber
+                }
             });
         });
     });
@@ -16,4 +23,21 @@ describe('Billing: OverviewCtrl', function () {
         expect(ctrl).to.exist;
     });
 
+    it('OverviewCtrl should have a sort object defined', function () {
+        expect(scope.sort).to.be.a('object');
+        expect(scope.sort).to.have.property('field');
+        expect(scope.sort.field).to.eq('date');
+    });
+
+    it('OverviewCtrl should have a pager defined by PageTracking', function () {
+        expect(scope.pager).to.be.an.instanceof(PageTrackingObject);
+    });
+
+    it('OverviewCtrl should have transaction data types defined', function () {
+        expect(scope.transactionData.types).to.be.an('array');
+        expect(scope.transactionData.types).to.have.length.above(0);
+
+        expect(scope.transactionData.status).to.be.an('array');
+        expect(scope.transactionData.status).to.have.length.above(0);
+    });
 });
