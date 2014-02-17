36f50ca23ad267256615fcbbd1bbf660b207ee12
Chris Cantu <christopher.cantu@gmail.com>
2014-02-16 22:26:21 -0700

    Added documentation for filters

--- app/scripts/billing/billingFilters.js
+++ app/scripts/billing/billingFilters.js
@@ -1,4 +1,19 @@
 angular.module('billingApp')
+    /**
+    * @ngdoc filter
+    * @name encore.filter:TransactionTable
+    * @description
+    * Filter which refines the collection of transactions based on the criteria provided. If no criteria are selected,
+    * the filter will return the entire collection of transactions.  The filter leverages an 'AND' search so as more
+    * criteria are selected more results will be filtered out.
+    *
+    * @param {Array} transactions - collection of transactions to be filtered.
+    * @param {Object} filter - Object which includes the criteria for filtering the list of transactions.
+    *     - **reference** {String} - Reference id for the transaction.
+    *     - **type** {String} - Type of the transaction.
+    *     - **status** {String} - Current state of the transaction.
+    *     - **date** {Date} - The date the transaction occurred.
+    */
     .filter('TransactionTable', function () {
         return function (transactions, filter) {
             filter = filter ? filter : {};
