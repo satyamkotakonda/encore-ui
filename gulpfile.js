94829778398995985e6bed962738970fd8f047d4
Chris Cantu <christopher.cantu@gmail.com>
2014-02-21 11:54:57 -0800

    Added less compiling and jshint.  JSCS plugin has a bug in it

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
     karma       = require('gulp-karma'),
+    less        = require('gulp-less'),
+    jshint      = require('gulp-jshint'),
+    jscs        = require('gulp-jscs'),
+    stylish     = require('jshint-stylish'),
     livereload  = require('gulp-livereload'),
     express     = require('express'),
     request   = require('request'),
@@ -8,13 +13,12 @@ var gulp        = require('gulp'),
     service     = new Stubby(),
     mockApi     = require('./test/api-mocks/requests/billing.js');
 
-
 var startServer = function () {
     var port = 8080,
         app = express();
 
     app.all('/api/*', function(req, res) {
-        console.log(req.url);
+        console.info('Mocked: ', req.url);
         var url = 'http://localhost:3000/api/' + req.url;
         req.pipe(request(url)).pipe(res);
     });
@@ -30,6 +34,13 @@ var startServer = function () {
     console.log('Listening on port: ' +  port);
 };
 
+gulp.task('less', function () {
+    return gulp.src('app/styles/*.less')
+        .pipe(concat('app.css'))
+        .pipe(less({}))
+        .pipe(gulp.dest('app/styles/'));
+});
+
 gulp.task('stubApi', function () {
     service.start({
         stubs: 3000,
@@ -39,6 +50,22 @@ gulp.task('stubApi', function () {
             console.error(error);
         }
     });
+});
+
+gulp.task('jshint', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('jscs', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jscs());
+});
+
+gulp.task('lint', function () {
+    gulp.run('jshint');
+    //gulp.run('jscs'); // commented out due to error in plugin
 });
 
 gulp.task('test', function () {
@@ -56,14 +83,19 @@ gulp.task('server', function () {
 });
 
 gulp.task('default', function () {
+    gulp.run('lint');
     gulp.run('test');
     gulp.run('stubApi');
     startServer();
     var server = livereload();
 
-    gulp.watch('app/**/*.js', ['test'])
+    gulp.watch('app/**/*.js', ['lint','test'])
         .on('change', function (file) {
-            console.log(file);
+            server.changed(file.path);
+        });
+
+    gulp.watch('app/styles/*.less', ['less'])
+        .on('change', function (file) {
             server.changed(file.path);
         });
 });
