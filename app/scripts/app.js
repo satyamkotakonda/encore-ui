7b4fe43c6d693d5bf2bae8e0211136d8d04d151d
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-24 11:32:09 -0600

    include sortableColumns`

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui.tpls', 'encore.ui.rxForm', 'encore.ui.rxPaginate',
-        'encore.ui.rxModalAction', 'billingSvcs'])
+        'encore.ui.rxModalAction', 'encore.ui.rxSortableColumn', 'billingSvcs'])
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/billing/overview/:accountNumber', {
