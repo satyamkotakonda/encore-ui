e1a38501d036cb48b1e1fe07f558deff74bee9ef
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-26 12:38:36 -0600

    rxSwitch directive for showing a toggable switch (readonly option as well)

---
+++ app/scripts/rxSwitch/rxSwitch.js
@@ -0,0 +1,27 @@
+angular.module('billing.ui.rxSwitch', [])
+/**
+ *
+ * @ngdoc directive
+ * @name encore.ui.rxSwitch
+ * @restrict E
+ * @description
+ * Displays an on/off switch toggle
+ */
+.directive('rxSwitch', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/scripts/rxSwitch/templates/rxSwitch.html',
+        scope: {
+            model: '=',
+            readonly: '@'
+        },
+        controller: function ($scope) {
+            $scope.switch = function (model) {
+                if ($scope.readonly && $scope.readonly === 'true') {
+                    return;
+                }
+                $scope.model = !!!model;
+            };
+        }
+    };
+});
