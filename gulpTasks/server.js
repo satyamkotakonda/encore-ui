58c03a1537e1a17235307b53284768ab5f0cdf3f
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-28 15:38:57 -0600

    replace console.log with log require, to use the same logging module

--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -1,16 +1,17 @@
 var express = require('express'),
     consolidate = require('consolidate'),
-    request = require('request');
+    request = require('request'),
+    log = require('./log');
 
 module.exports = function (cb) {
     var port = 9000,
         app = express(),
         appDirectory = __dirname.replace('gulpTasks', 'app');
 
-    console.log(__dirname, appDirectory);
+    log(__dirname, appDirectory);
 
     app.all('/api/*', function (req, res) {
-        console.info('Mocked: ', req.url);
+        log('Mocked: ', req.url);
         var url = 'http://localhost:3000/api/' + req.url;
         req.pipe(request(url)).pipe(res);
     });
@@ -23,6 +24,6 @@ module.exports = function (cb) {
     });
 
     app.listen(port);
-    console.log('Listening on port: ' +  port);
+    log('Listening on port: ' +  port);
     cb();
 };
