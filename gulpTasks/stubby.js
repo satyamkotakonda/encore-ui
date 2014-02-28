b52efdbeaef576f199ddedb28fcabbd52b72105f
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-28 15:12:03 -0600

    added callback that will be passed by gulp, run at the end to notify that the task is done running

--- gulpTasks/stubby.js
+++ gulpTasks/stubby.js
@@ -2,14 +2,15 @@ var Stubby = require('stubby').Stubby,
     service = new Stubby(),
     mockApi = require('./../test/api-mocks/requests/billing.js');
 
-module.exports = function () {
+module.exports = function (cb) {
     service.start({
         stubs: 3000,
         data: mockApi
     }, function (error) {
-        if(error) {
+        if (error) {
             console.error(error);
             throw new Error(error);
         }
     });
+    cb();
 };
