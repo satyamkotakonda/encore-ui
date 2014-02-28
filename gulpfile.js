943a6aacc1b628a552610fc5a52a58ae0029c2e0
Freddy Knuth <freddy.knuth@rackspace.com>
2014-02-28 15:12:19 -0600

    pulled out the gulp task details out into their own files.  This is a first step, there's likely a better way

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,21 @@
 var gulp        = require('gulp'),
-    concat      = require('gulp-concat'),
-    karma       = require('gulp-karma'),
-    less        = require('gulp-less'),
-    jshint      = require('gulp-jshint'),
-    jscs        = require('gulp-jscs'),
     livereload  = require('gulp-livereload'),
-    open        = require('gulp-open'),
-    stylish     = require('jshint-stylish'),
-    stubby      = require('./gulpTasks/stubby'),
-    server      = require('./gulpTasks/server');
+    open        = require('gulp-open');
 
-gulp.task('less', function () {
-    return gulp.src('app/styles/*.less')
-        .pipe(concat('app.css'))
-        .pipe(less({}))
-        .pipe(gulp.dest('app/styles/'));
-});
-
-gulp.task('stubApi', function () {
-    stubby();
-});
-
-gulp.task('jshint', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter(stylish));
-});
-
-gulp.task('jscs', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe(jscs());
-});
-
-gulp.task('lint', function () {
-    gulp.run('jshint');
-    gulp.run('jscs');
-});
-
-gulp.task('test', function () {
-    return gulp.src('app/app.js')
-        .pipe(karma({
-            configFile: './karma.conf.js',
-            action: 'run',
-            reporters: ['spec','coverage','threshold']
-        }));
-});
-
-gulp.task('server', function () {
-    gulp.run('stubApi');
-    server();
-    return;
-});
+gulp.task('less', require('./gulpTasks/less'));
+gulp.task('jshint', require('./gulpTasks/jshint'));
+gulp.task('jscs', require('./gulpTasks/jscs'));
+gulp.task('lint', ['jshint', 'jscs']);
+gulp.task('test', require('./gulpTasks/karma'));
+gulp.task('stubApi', require('./gulpTasks/stubby'));
+gulp.task('server', ['stubApi'], require('./gulpTasks/server'));
 
 gulp.task('open', function () {
     return gulp.src('app/index.html')
-        .pipe(open('', {url: 'http://localhost:9000', app: 'google chrome'}));
+        .pipe(open('', { url: 'http://localhost:9000', app: 'google chrome' }));
 });
 
-gulp.task('default', function () {
-    gulp.run('server');
-    gulp.run('lint');
-    gulp.run('test');
-    gulp.run('open');
+gulp.task('default', ['lint', 'test', 'less', 'server'], function () {
     var server = livereload();
 
     gulp.watch('app/**/*.js', ['lint','test'])
@@ -73,4 +27,8 @@ gulp.task('default', function () {
         .on('change', function (file) {
             server.changed(file.path);
         });
+    
+    gulp.run('open');
+
+    //cb();
 });
