ff1e333ab1dbc82e115584e97cf5f9f19efc19b3
Freddy Knuth <freddy.knuth@rackspace.com>
2014-03-12 17:26:33 -0500

    options controller and test file (TBD)

---
+++ app/scripts/payment/optionsCtrl.js
@@ -0,0 +1,29 @@
+angular.module('billingApp')
+    /**
+    * @ngdoc object
+    * @name encore:controller.OptionsCtrl
+    * @description
+    * The Controller which displays an overview of a users' billing info.
+    *
+    * @example
+    * <pre>
+    * .controller('OverviewCtrl', function ($scope, $routeParams, Transaction,
+    *       Account, Period, PageTracking)
+    * </pre>
+    */
+    .controller('OptionsCtrl', function ($scope, $routeParams, Account, PaymentMethod,
+            DefaultPaymentMethod, rxSortUtil) {
+
+        var getDefaultMethod = function () {
+                $scope.defaultMethod = DefaultPaymentMethod($scope.paymentMethods);
+            },
+            sortCol = function (predicate) {
+                return rxSortUtil.sortCol($scope, predicate);
+            };
+
+        $scope.sortCol = sortCol;
+        $scope.sort = rxSortUtil.getDefault('isDefault');
+        $scope.account = Account.get({ id: $routeParams.accountNumber });
+        $scope.paymentMethods = PaymentMethod.list({ id: $routeParams.accountNumber }, getDefaultMethod);
+
+    });
