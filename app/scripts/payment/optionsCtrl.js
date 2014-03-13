9d9c575f81566ad0fa74a9aab6efc11db0d8e303
Freddy Knuth <freddy.knuth@rackspace.com>
2014-03-13 12:31:30 -0500

    use the passed in attribute than waiting for the scope to be updated

--- app/scripts/payment/optionsCtrl.js
+++ app/scripts/payment/optionsCtrl.js
@@ -14,8 +14,8 @@ angular.module('billingApp')
     .controller('OptionsCtrl', function ($scope, $routeParams, Account, PaymentMethod,
             DefaultPaymentMethod, rxSortUtil) {
 
-        var getDefaultMethod = function () {
-                $scope.defaultMethod = DefaultPaymentMethod($scope.paymentMethods);
+        var getDefaultMethod = function (paymentMethods) {
+                $scope.defaultMethod = DefaultPaymentMethod(paymentMethods);
             },
             sortCol = function (predicate) {
                 return rxSortUtil.sortCol($scope, predicate);
@@ -25,5 +25,4 @@ angular.module('billingApp')
         $scope.sort = rxSortUtil.getDefault('isDefault');
         $scope.account = Account.get({ id: $routeParams.accountNumber });
         $scope.paymentMethods = PaymentMethod.list({ id: $routeParams.accountNumber }, getDefaultMethod);
-
     });
