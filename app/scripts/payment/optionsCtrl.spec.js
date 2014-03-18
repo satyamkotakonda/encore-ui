f6affe97c18d63900b091a68fa37a712b0bb0989
Freddy Knuth <freddy.knuth@rackspace.com>
2014-03-18 16:32:55 -0500

    more tests for colSort

--- app/scripts/payment/optionsCtrl.spec.js
+++ app/scripts/payment/optionsCtrl.spec.js
@@ -4,7 +4,7 @@ describe('OptionsCtrl', function () {
     var testAccountNumber = '12345',
         paymentMethods = [{
             'id': 'urn:uuid:f47ac10b-58cc-4372-a567-0e02b2c3d479',
-            'isDefault': 'true',
+            'isDefault': true,
             'paymentCard': {
                 'cardNumber': 'XXXXXXXXXXXX3456',
                 'cardType': 'VISA'
@@ -14,18 +14,24 @@ describe('OptionsCtrl', function () {
     beforeEach(function () {
         module('billingApp');
 
-        inject(function ($controller, $rootScope, Account, PaymentMethod,
+        inject(function ($controller, $rootScope, $q, Account, PaymentMethod,
             DefaultPaymentMethod) {
+            var getResourceMock = function (data) {
+                var deferred = $q.defer();
+                data.$promise = deferred.promise;
+                data.$deferred = deferred;
+                return data;
+            };
 
             scope = $rootScope.$new();
             account = Account;
             paymentMethod = PaymentMethod;
             defaultPaymentMethod = DefaultPaymentMethod;
 
-            account.get = sinon.stub(account, 'get');
+            account.get = sinon.stub(account, 'get').returns(getResourceMock({}));
             paymentMethod.list = function (param, success) {
                 success(paymentMethods);
-                return paymentMethods;
+                return getResourceMock(paymentMethods);
             };
 
             ctrl = $controller('OptionsCtrl', {
@@ -48,9 +54,18 @@ describe('OptionsCtrl', function () {
         expect(scope.cardSort.predicate).to.eq('isDefault');
     });
 
-    it('OptionsCtrl should return a payment card sorting predicate when calling sortCol', function () {
-        scope.cardSortCol('isDefault');
-        expect(scope.cardSort.predicate).to.be.eq('isDefault');
+    it('OptionsCtrl cardSortCol should change predicate when called with paymentCard.cardNumber', function () {
+        scope.cardSortCol('paymentCard.cardNumber');
+        expect(scope.cardSort.predicate).to.be.eq('paymentCard.cardNumber');
+    });
+
+    it('OptionsCtrl cardSortCol should change reverse sort when called twice with the same col', function () {
+        scope.cardSortCol('paymentCard.cardNumber');
+        expect(scope.cardSort.predicate).to.be.eq('paymentCard.cardNumber');
+        expect(scope.cardSort.reverse).to.be.eq(false);
+        scope.cardSortCol('paymentCard.cardNumber');
+        expect(scope.cardSort.predicate).to.be.eq('paymentCard.cardNumber');
+        expect(scope.cardSort.reverse).to.be.eq(true);
     });
 
     it('OptionsCtrl should have a sort object for ACH Accounts defined', function () {
@@ -59,9 +74,18 @@ describe('OptionsCtrl', function () {
         expect(scope.achSort.predicate).to.eq('isDefault');
     });
 
-    it('OptionsCtrl should return an ACH Account sorting predicate when calling sortCol', function () {
-        scope.achSortCol('isDefault');
-        expect(scope.achSort.predicate).to.be.eq('isDefault');
+    it('OptionsCtrl achSortCol should change predicate when called with electronicCheck.accountType', function () {
+        scope.achSortCol('electronicCheck.accountType');
+        expect(scope.achSort.predicate).to.be.eq('electronicCheck.accountType');
+    });
+
+    it('OptionsCtrl achSortCol should change reverse sort when called twice with the same col', function () {
+        scope.achSortCol('electronicCheck.accountType');
+        expect(scope.achSort.predicate).to.be.eq('electronicCheck.accountType');
+        expect(scope.achSort.reverse).to.be.eq(false);
+        scope.achSortCol('electronicCheck.accountType');
+        expect(scope.achSort.predicate).to.be.eq('electronicCheck.accountType');
+        expect(scope.achSort.reverse).to.be.eq(true);
     });
 
     it('OptionsCtrl should get account info', function () {
