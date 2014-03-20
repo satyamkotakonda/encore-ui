3849437e8d5f5249cc1de6356d979d764779c666
Freddy Knuth <freddy.knuth@rackspace.com>
2014-03-20 17:09:06 -0500

    common function for getting the list of payment methods. functions for posting payment and changing default payment

--- app/scripts/payment/optionsCtrl.js
+++ app/scripts/payment/optionsCtrl.js
@@ -12,17 +12,56 @@ angular.module('billingApp')
     * </pre>
     */
     .controller('OptionsCtrl', function ($scope, $routeParams, $q, Account, PaymentMethod,
-            DefaultPaymentMethod, rxSortUtil, rxPromiseNotifications, STATUS_MESSAGES) {
+            Payment, DefaultPaymentMethod, rxSortUtil, rxPromiseNotifications, STATUS_MESSAGES) {
 
         var getDefaultMethod = function (paymentMethods) {
                 $scope.defaultMethod = DefaultPaymentMethod(paymentMethods);
+            },
+            listPaymentMethods = function () {
+                return PaymentMethod.list({
+                    id: $routeParams.accountNumber,
+                    showDisabled: true
+                }, getDefaultMethod);
             },
             sortCol = function (sort) {
                 return function (predicate) {
                     var reverse = ($scope[sort].predicate === predicate) ? !$scope[sort].reverse : false;
                     $scope[sort] = { reverse: reverse, predicate: predicate };
                 };
+            },
+            changeDefaultMethod = function (methodId) {
+                var changeDefaultResult = PaymentMethod.changeDefault({
+                    id: $routeParams.accountNumber
+                }, {
+                    defaultMethod: {
+                        methodId: methodId
+                    }
+                }, function () {
+                    $scope.paymentMethods = listPaymentMethods();
+                });
+                rxPromiseNotifications.add(changeDefaultResult.$promise, {
+                    loading: STATUS_MESSAGES.payment.load,
+                    success: STATUS_MESSAGES.payment.success,
+                    error: STATUS_MESSAGES.payment.error
+                }, 'changeDefault');
+            },
+            postPayment = function (amount, methodId) {
+                var paymentResult = Payment.post({
+                    id: $routeParams.accountNumber,
+                    payment: {
+                        amount: amount,
+                        methodId: methodId
+                    }
+                });
+                rxPromiseNotifications.add(paymentResult.$promise, {
+                    loading: STATUS_MESSAGES.payment.load,
+                    success: STATUS_MESSAGES.payment.success,
+                    error: STATUS_MESSAGES.payment.error
+                }, 'makePayment');
             };
+
+        $scope.changeDefaultMethod = changeDefaultMethod;
+        $scope.postPayment = postPayment;
 
         $scope.cardSortCol = sortCol('cardSort');
         $scope.cardSort = rxSortUtil.getDefault('isDefault', true);
@@ -31,7 +70,8 @@ angular.module('billingApp')
         $scope.achSort = rxSortUtil.getDefault('isDefault', true);
 
         $scope.account = Account.get({ id: $routeParams.accountNumber });
-        $scope.paymentMethods = PaymentMethod.list({ id: $routeParams.accountNumber }, getDefaultMethod);
+        $scope.paymentMethods = listPaymentMethods();
+
         rxPromiseNotifications.add($q.all([
             $scope.account.$promise,
             $scope.paymentMethods.$promise
