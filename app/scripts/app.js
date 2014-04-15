6b77a04ae1a9062780d56c13e98bc9a388af32c1
Freddy Knuth <freddy.knuth@rackspace.com>
2014-04-15 16:26:52 -0500

    implementing encore.ui new custom menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,37 @@
 'use strict';
-angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui.tpls', 'encore.ui.rxForm', 'encore.ui.rxPaginate',
-        'encore.ui.rxModalAction', 'encore.ui.rxSortableColumn', 'encore.ui.rxNotify', 'rxSwitch',
-        'encore.ui.rxPopover', 'billingSvcs', 'paymentSvcs', 'constants', 'productConstants'])
-    .run(function ($http) {
+var accountNumber = '020-5955321';
+angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.tpls', 'encore.ui.rxForm',
+        'encore.ui.rxPaginate', 'encore.ui.rxModalAction', 'encore.ui.rxSortableColumn', 'encore.ui.rxNotify',
+        'rxSwitch', 'encore.ui.rxPopover', 'billingSvcs', 'paymentSvcs', 'constants', 'productConstants'])
+    .run(function ($http, $rootScope) {
         //TODO: Integrate rxAuth/rxLogin so that we no longer have to temporarily store the token key
         $http.defaults.headers.common['X-Auth-Token'] = '2184d781eafa4c949e9d68df6c75f818';
         // Forces JSON only
         $http.defaults.headers.common['Accept'] = 'application/json';
+
+        var appName = 'billing',
+            appRoute = '/' + appName;
+
+        $rootScope.billingMenu = [{
+            title: 'Example Menu',
+            children: [
+                {
+                    href: appRoute + '/overview/' + accountNumber,
+                    linkText: 'Billing Overview'
+                },
+                {
+                    href: appRoute + '/usage/' + accountNumber,
+                    linkText: 'Usages & Charges'
+                },
+                {
+                    href: appRoute + '/payment/' + accountNumber + '/options',
+                    linkText: 'Payment Options'
+                }
+            ]
+        }];
     })
     .config(function ($routeProvider, $locationProvider) {
         //TODO: To be replaced once account search is implemented, only temporary for dev
-        var accountNumber = '020-5955321';
         $routeProvider
             .when('/billing/overview/:accountNumber', {
                 templateUrl: '/views/billing/overview.html',
