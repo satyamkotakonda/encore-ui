f7d8c9f70fdc4a10b72864e04af0a240a752b50e
Freddy Knuth <freddy.knuth@rackspace.com>
2014-04-22 10:20:52 -0500

    fixed issues with building out the menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,33 @@
 'use strict';
-var accountNumber = '020-5955321';
 angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.tpls', 'encore.ui.rxForm',
         'encore.ui.rxPaginate', 'encore.ui.rxModalAction', 'encore.ui.rxSortableColumn', 'encore.ui.rxNotify',
         'rxSwitch', 'encore.ui.rxPopover', 'billingSvcs', 'paymentSvcs', 'constants', 'productConstants'])
-    .run(function ($http) {
-        //TODO: Integrate rxAuth/rxLogin so that we no longer have to temporarily store the token key
-        $http.defaults.headers.common['X-Auth-Token'] = 'f59469862eb74aac8d7e1aaa5c088765';
+    .run(function ($http, $rootScope) {
+        //#TODO: Integrate rxAuth/rxLogin so that we no longer have to temporarily store the token key
+        $http.defaults.headers.common['X-Auth-Token'] = 'b7c72d31c6a748da9ba0e6200dd680e7';
         // Forces JSON only
         $http.defaults.headers.common['Accept'] = 'application/json';
+
+        $rootScope.billingMenu = [{
+            title: 'Billing',
+            children: [
+                {
+                    href: { tld: 'billing', path: 'billing/overview/{{accountNumber}}' },
+                    linkText: 'Overview'
+                },
+                {
+                    href: { tld: 'billing', path: 'billing/usage/{{accountNumber}}' },
+                    linkText: 'Usages & Charges'
+                },
+                {
+                    href: { tld: 'billing', path: 'billing/payment/{{accountNumber}}/options' },
+                    linkText: 'Payment Options'
+                }
+            ]
+        }];
     })
     .config(function ($routeProvider, $locationProvider) {
-        //TODO: To be replaced once account search is implemented, only temporary for dev
+        //#TODO: To be replaced once account search is implemented, only temporary for dev
         $routeProvider
             .when('/billing/overview/:accountNumber', {
                 templateUrl: '/views/billing/overview.html',
@@ -25,36 +42,9 @@ angular.module('billingApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.t
                 controller: 'OptionsCtrl'
             })
             .otherwise({
-                //this is temporary until we get a more solid solution
-                redirectTo: '/billing/overview/' + accountNumber
+                //#TODO: this is temporary until we get a more solid solution
+                redirectTo: '/billing/overview/020-5955321'
             });
 
         $locationProvider.html5Mode(true).hashPrefix('!');
-    })
-    .controller('navBillingCtrl', function ($scope, $routeParams, $route) {
-        console.log('HUSSAM!');
-        var appName = 'billing',
-            appRoute = '/' + appName;
-        $scope.params = $routeParams;
-        $scope.$watch('params', function () {
-            var accountNumber = $routeParams.accountNumber;
-            $scope.billingMenu = [{
-                title: 'Billing',
-                children: [
-                    {
-                        href: appRoute + '/overview/' + accountNumber,
-                        linkText: 'Overview'
-                    },
-                    {
-                        href: appRoute + '/usage/' + accountNumber,
-                        linkText: 'Usages & Charges'
-                    },
-                    {
-                        href: appRoute + '/payment/' + accountNumber + '/options',
-                        linkText: 'Payment Options'
-                    }
-                ]
-            }];
-        }, true);
-
     });
