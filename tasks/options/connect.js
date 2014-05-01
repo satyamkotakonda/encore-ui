40c18d2018ef2822b0383f987ef82032e8f0eb84
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-01 10:45:55 -0500

    fixes to build pieces

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -8,42 +8,44 @@ module.exports = {
         port: 9000,
         hostname: 'localhost'
     },
-    proxies: [{
-        context: '/' + config.appName,
-        host: 'localhost',
-        port: 9000,
-        rewrite: appLocalRewrite
-    },
-    {
-        context: '/api/billing',
-        host: 'staging.billingv2.pipeline2.api.rackspacecloud.com',
-        port: 443,
-        https: true,
-        changeOrigin: true,
-        rewrite: {
-            '/api/billing': '/v2/accounts'
+    proxies: [
+        {
+            context: '/' + config.appName,
+            host: 'localhost',
+            port: 9000,
+            rewrite: appLocalRewrite
+        },
+        {
+            context: '/api/billing',
+            host: 'staging.billingv2.pipeline2.api.rackspacecloud.com',
+            port: 443,
+            https: true,
+            changeOrigin: true,
+            rewrite: {
+                '/api/billing': '/v2/accounts'
+            }
+        },
+        {
+            context: '/api/payment',
+            host: 'staging.system.payment.pipeline2.api.rackspacecloud.com',
+            port: 443,
+            https: true,
+            changeOrigin: true,
+            rewrite: {
+                '/api/payment': '/v1/accounts'
+            }
+        },
+        {
+            context: '/api',
+            host: 'localhost',
+            port: 3000,
+            https: false,
+            changeOrigin: false,
+            rewrite: {
+                '/api': '/api'
+            }
         }
-    },
-    {
-        context: '/api/payment',
-        host: 'staging.system.payment.pipeline2.api.rackspacecloud.com',
-        port: 443,
-        https: true,
-        changeOrigin: true,
-        rewrite: {
-            '/api/payment': '/v1/accounts'
-        }
-    },
-    {
-        context: '/api',
-        host: 'localhost',
-        port: 3000,
-        https: false,
-        changeOrigin: false,
-        rewrite: {
-            '/api': '/api'
-        }
-    }],
+    ],
     livereload: {
         options: {
             middleware: function (cnct) {
@@ -74,10 +76,6 @@ module.exports = {
         options: {
             middleware: function (cnct) {
                 return [
-                    config.proxyRequest,
-                    config.modRewrite(['!\\.[0-9a-zA-Z_-]+$ /index.html']),
-                    config.liveReloadPage,
-                    config.mountFolder(cnct, '.tmp'),
                     config.mountFolder(cnct, config.appDest)
                 ];
             }
