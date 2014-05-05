3b6ad110358ed022dd17ebf9788cee672beb4b45
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-05 15:46:04 -0500

    transactionDetails Controller & View

---
+++ app/scripts/billing/transactionDetailsCtrl.js
@@ -0,0 +1,47 @@
+angular.module('billingApp')
+    /**
+    * @ngdoc object
+    * @name encore:controller.TransactionsCtrl
+    * @description
+    * The Controller which displays an overview of a users' billing info.
+    *
+    * @requires $scope - The $scope variable for interacting with the UI.
+    * @requires $routeParams - AngularJS service which provides access to route paramters
+    * @requires billingSvcs.Transaction - Service for CRUD operations for the Transaction resource.
+    * @requires billingSvcs.Account - Service for CRUD operations for the Account resource.
+    * @requires encore.rxSortableColumn:rxSortUtil - Service which provides column sort related functions
+    * @requires billingSvcs.DATE_FORMAT - Constant that defines the default format for dates
+    *
+    * @example
+    * <pre>
+    * .controller('TransactionsCtrl', function ($scope, $routeParams, $q, Transaction, Account, Balance,
+    *   Period, Payment, PaymentMethod, PageTracking, rxSortUtil, rxPromiseNotifications,
+    *   DefaultPaymentMethodFilter,
+    *   DATE_FORMAT, TRANSACTION_TYPES, TRANSACTION_STATUSES, STATUS_MESSAGES)
+    * </pre>
+    */
+    .controller('TransactionDetailsCtrl', function ($scope, $routeParams, Transaction, Account,
+        rxSortUtil, DATE_FORMAT) {
+
+        // Action for setting the sort
+        var sortCol = function (predicate) {
+                return rxSortUtil.sortCol($scope, predicate);
+            };
+
+        // Pass the account number for building links
+        $scope.accountNumber = $routeParams.accountNumber;
+
+        // Set the default sort of the transactions
+        $scope.sort = rxSortUtil.getDefault('date', true);
+        $scope.sortCol = sortCol;
+
+        // Default Date Format
+        $scope.defaultDateFormat = DATE_FORMAT;
+
+        // Get Account & Transactions Info
+        $scope.transactions = Transaction.get({
+            id: $routeParams.accountNumber,
+            transactionNumber: $routeParams.transactionNumber,
+            transactionType: $routeParams.transactionType
+        });
+    });
---
+++ app/scripts/billing/transactionDetailsCtrl.spec.js
@@ -0,0 +1,61 @@
+describe('Billing: TransactionDetailsCtrl', function () {
+    var scope, ctrl, account, transaction;
+
+    var testAccountNumber = '12345';
+
+    beforeEach(function () {
+        module('billingApp');
+
+        inject(function ($controller, $rootScope, Account, Transaction, PageTracking, $q) {
+            var getResourceResultMock = function (data) {
+                    var deferred = $q.defer();
+                    data.$promise = deferred.promise;
+                    data.$deferred = deferred;
+                    return data;
+                },
+                getResourceMock = function (returnData) {
+                    returnData = getResourceResultMock(returnData);
+                    return function (callData, success, error) {
+                        returnData.$promise.then(success, error);
+                        return returnData;
+                    };
+                };
+            scope = $rootScope.$new();
+            transaction = Transaction;
+            account = Account;
+
+            account.get = sinon.stub(account, 'get', getResourceMock({}));
+            transaction.get = sinon.stub(transaction, 'get', getResourceMock({}));
+
+            ctrl = $controller('TransactionDetailsCtrl',{
+                $scope: scope,
+                Transaction: transaction,
+                Account: account,
+                $routeParams: { accountNumber: testAccountNumber }
+            });
+        });
+    });
+
+    it('TransactionDetailsCtrl should exist', function () {
+        expect(ctrl).to.exist;
+    });
+
+    it('TransactionDetailsCtrl should have a sort object defined', function () {
+        expect(scope.sort).to.be.a('object');
+        expect(scope.sort).to.have.property('predicate');
+        expect(scope.sort.predicate).to.eq('date');
+    });
+
+    it('TransactionDetailsCtrl should have a default date format', function () {
+        expect(scope.defaultDateFormat).to.be.eq('MM / dd / yyyy');
+    });
+
+    it('TransactionDetailsCtrl should get account info', function () {
+        sinon.assert.calledOnce(transaction.get);
+    });
+
+    it('TransactionDetailsCtrl should return a sorting predicate when calling sortCol', function () {
+        scope.sortCol('date');
+        expect(scope.sort.predicate).to.be.eq('date');
+    });
+});
