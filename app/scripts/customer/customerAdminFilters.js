c32bb5c50dafde155ace810d06d80a128bccafc2
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-09 10:41:30 -0500

    Filters/Resources for support/customer level API's

---
+++ app/scripts/customer/customerAdminFilters.js
@@ -0,0 +1,43 @@
+angular.module('billingApp')
+    /**
+    * @ngdoc filter
+    * @name encore.filter:ContactType
+    * @description
+    * Filter a list of account contacts by a specific type.
+    *
+    * @param {Array} contacts - collection of Contacts to be filtered.
+    * @param {String} filterType - Key to filter by
+    */
+    .filter('ContactType', function () {
+        return function (contacts, filterType) {
+            return _.filter(contacts, function (contact) {
+                return _.contains(contact.roles.role, filterType);
+            });
+        };
+    })
+    /**
+    * @ngdoc filter
+    * @name encore.filter:PrimaryAddress
+    * @description
+    * Filter a list of contacts addresses by primary.
+    *
+    * @param {Array} contacts - collection of addresses to be filtered.
+    */
+    .filter('PrimaryAddress', function () {
+        return function (addresses) {
+            return _.find(addresses, { primary: true });
+        };
+    })
+    /**
+    * @ngdoc filter
+    * @name encore.filter:PrimaryEmail
+    * @description
+    * Filter a list of contacts email addresses by primary.
+    *
+    * @param {Array} contacts - collection of addresses to be filtered.
+    */
+    .filter('PrimaryEmail', function () {
+        return function (addresses) {
+            return _.find(addresses, { primary: true });
+        };
+    });
---
+++ app/scripts/support/supportFilters.js
@@ -0,0 +1,17 @@
+angular.module('billingApp')
+    /**
+    * @ngdoc filter
+    * @name encore.filter:RoleName
+    * @description
+    * Filter a list of account roles by a specific name.
+    *
+    * @param {Array} roles - collection of Contacts to be filtered.
+    * @param {String} name - Key to filter by
+    */
+    .filter('RoleName', function () {
+        return function (roles, name) {
+            return _.filter(roles, function (role) {
+                return _.contains(role.role, name);
+            });
+        };
+    });
