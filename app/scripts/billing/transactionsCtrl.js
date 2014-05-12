49ac1e2da28e70bb8ca02ee461242ad41125c22a
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-12 10:59:10 -0500

    updated to use the getRAN/RCN functions until we can do these directly in the resource

--- app/scripts/billing/transactionsCtrl.js
+++ app/scripts/billing/transactionsCtrl.js
@@ -33,8 +33,14 @@ angular.module('billingApp')
     */
     .controller('TransactionsCtrl', function ($scope, $routeParams, $q, Transaction, Account, Balance,
         Period, Payment, PaymentMethod, PageTracking, PaymentInfo, BillInfo,
-        rxSortUtil, rxPromiseNotifications, DefaultPaymentMethodFilter,
+        rxSortUtil, rxPromiseNotifications, DefaultPaymentMethodFilter, AccountNumberUtil,
         DATE_FORMAT, TRANSACTION_TYPES, TRANSACTION_STATUSES, STATUS_MESSAGES) {
+
+        var RAN = AccountNumberUtil.getRAN($routeParams.accountNumber),
+            RCN = AccountNumberUtil.getRCN($routeParams.accountNumber),
+            accountType = AccountNumberUtil.getAccountType($routeParams.accountNumber);
+
+        $scope.accountNumber = $routeParams.accountNumber;
 
         // Action for clearing the filters
         var resetPager = function () {
@@ -57,7 +63,7 @@ angular.module('billingApp')
             },
             postPayment = function (amount, methodId) {
                 $scope.paymentResult = Payment.post({
-                    id: $routeParams.accountNumber,
+                    id: RAN,
                     payment: {
                         amount: amount,
                         methodId: methodId
@@ -86,16 +92,13 @@ angular.module('billingApp')
         $scope.clearFilter = clearFilter;
 
         // Get Account & Transactions Info
-        $scope.billInfo = BillInfo.get({ id: $routeParams.accountNumber });
-        $scope.paymentInfo = PaymentInfo.get({ id: $routeParams.accountNumber });
-        $scope.account = Account.get({ id: $routeParams.accountNumber });
-        $scope.balance = Balance.get({ id: $routeParams.accountNumber });
-        $scope.transactions = Transaction.list({ id: $routeParams.accountNumber });
-        $scope.paymentMethods = PaymentMethod.list({ id: $routeParams.accountNumber });
-        $scope.billingPeriods = Period.list({ id: $routeParams.accountNumber });
-
-        // Pass the account number for building links
-        $scope.accountNumber = $routeParams.accountNumber;
+        $scope.billInfo = BillInfo.get({ id: RAN });
+        $scope.paymentInfo = PaymentInfo.get({ id: RAN });
+        $scope.account = Account.get({ id: RCN, type: accountType });
+        $scope.balance = Balance.get({ id: RAN });
+        $scope.transactions = Transaction.list({ id: RAN });
+        $scope.paymentMethods = PaymentMethod.list({ id: RAN });
+        $scope.billingPeriods = Period.list({ id: RAN });
 
         // Group the promises in $q.all for a global error message if any errors occur
         rxPromiseNotifications.add($q.all([
--- app/scripts/payment/optionsCtrl.js
+++ app/scripts/payment/optionsCtrl.js
@@ -24,8 +24,10 @@ angular.module('billingApp')
     *       Payment, DefaultPaymentMethodFilter, rxSortUtil, rxPromiseNotifications, STATUS_MESSAGES) {
     * </pre>
     */
-    .controller('OptionsCtrl', function ($scope, $routeParams, $q, Account, Balance, PaymentMethod,
+    .controller('OptionsCtrl', function ($scope, $routeParams, $q, Account, Balance, PaymentMethod, AccountNumberUtil,
             Payment, DefaultPaymentMethodFilter, rxSortUtil, rxPromiseNotifications, STATUS_MESSAGES) {
+
+        var RAN = AccountNumberUtil.getRAN($routeParams.accountNumber);
 
         // Get filter the paymentMethods and retrieve the default one (callback)
         var getDefaultMethod = function (paymentMethods) {
@@ -38,7 +40,7 @@ angular.module('billingApp')
             // Return the list of payment methods for the account being viewed
             getPaymentMethods = function () {
                 return PaymentMethod.list({
-                    id: $routeParams.accountNumber,
+                    id: RAN,
                     showDisabled: true
                 }, getDefaultMethod);
             },
@@ -58,7 +60,7 @@ angular.module('billingApp')
             // methods upon success.  Passes promise to rxPromiseNotifications
             disableMethod = function (methodId) {
                 var disableMethodResult = PaymentMethod.disable({
-                    id: $routeParams.accountNumber,
+                    id: RAN,
                     methodId: methodId
                 }, refreshPaymentMethods);
                 // Display messages depending on the success of the call
@@ -72,7 +74,7 @@ angular.module('billingApp')
             // methods upon success.  Passes promise to rxPromiseNotifications
             changeDefaultMethod = function (methodId) {
                 var changeDefaultResult = PaymentMethod.changeDefault({
-                    id: $routeParams.accountNumber
+                    id: RAN
                 }, {
                     defaultMethod: {
                         methodId: methodId
@@ -89,7 +91,7 @@ angular.module('billingApp')
             // Passes promise to rxPromiseNotifications
             postPayment = function (amount, methodId) {
                 var paymentResult = Payment.post({
-                    id: $routeParams.accountNumber,
+                    id: RAN,
                     payment: {
                         amount: amount,
                         methodId: methodId
@@ -117,10 +119,10 @@ angular.module('billingApp')
         $scope.achSort = rxSortUtil.getDefault('isDefault', true);
 
         // Get Account Info
-        $scope.account = Account.get({ id: $routeParams.accountNumber });
+        $scope.account = Account.get({ id: RAN });
 
         // Get Account Info
-        $scope.balance = Balance.get({ id: $routeParams.accountNumber }, getCurrentDue);
+        $scope.balance = Balance.get({ id: RAN }, getCurrentDue);
 
         // Gets the payment methods
         $scope.paymentMethods = getPaymentMethods();
