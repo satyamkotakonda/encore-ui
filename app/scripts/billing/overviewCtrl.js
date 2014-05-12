d5789edcfda1e58297cf2d89c2e792878e612615
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-12 10:07:33 -0500

    updated use of services for the new overview page

--- app/scripts/billing/overviewCtrl.js
+++ app/scripts/billing/overviewCtrl.js
@@ -8,19 +8,20 @@ angular.module('billingApp')
     * @requires $scope - The $scope variable for interacting with the UI.
     * @requires $routeParams - AngularJS service which provides access to route paramters
     * @requires $q - AngularJS q implementation for working with promises
-    * @requires billingSvcs.Transaction - Service for CRUD operations for the Transaction resource.
-    * @requires billingSvcs.Account - Service for CRUD operations for the Account resource.
+    * @requires customerAdminSvcs.Account - Service for CRUD operations for the Account resource.
+    * @requires customerAdminSvcs.Contact - Service for CRUD operations for the Contact resource.
+    * @requires supportSvcs.SupportAccount - Service for CRUD operations for the SupportAccount resource.
+    * @requires supportSvcs.SupportRoles - Service for CRUD operations for the SupportRoles resource.
     * @requires billingSvcs.Balance - Service for CRUD operations for the Balance resource.
-    * @requires billingSvcs.Period - Service for CRUD operations for the Period resource.
     * @requires billingSvcs.Payment - Service for CRUD operations for the Payment resource.
     * @requires billingSvcs.PaymentMethod - Service for CRUD operations for the PaymentMethod resource.
-    * @requires encore.paginate:PageTracking - Service which creates an object for pagination.
-    * @requires encore.rxSortableColumn:rxSortUtil - Service which provides column sort related functions
+    * @requires billingSvcs.ContractEntity - Service for CRUD operations for the ContractEntity resource.
+    * @requires billingSvcs.SupportInfo - Service for CRUD operations for the SupportInfo resource.
     * @requires encore.rxNotify:rxPromiseNotifications - Service which provides notifications for promises
-    * @requires billingSvcs.DefaultPaymentMethodFilter - Filter which facilitates retrieval of default payment method
+    * @requires billingFilters.DefaultPaymentMethodFilter - Filter which facilitates retrieval of default payment method
+    * @requires customerAdminFilters.PrimaryAddressFilter - Filter which faciliates retrieval of the primary address
+    * @requires supportFilters.RoleNameFilter - Filter which filters by the type of role desired
     * @requires billingSvcs.DATE_FORMAT - Constant that defines the default format for dates
-    * @requires billingSvcs.TRANSACTION_TYPES - Constant list of the different types of transactions
-    * @requires billingSvcs.TRANSACTION_STATUSES - Constant list of different transaction statuses
     * @requires billingSvcs.STATUS_MESSAGES - Constant object defining messaging to be used throughout the app
     *
     * @example
@@ -31,24 +32,18 @@ angular.module('billingApp')
     *   DATE_FORMAT, TRANSACTION_TYPES, TRANSACTION_STATUSES, STATUS_MESSAGES)
     * </pre>
     */
-    .controller('OverviewCtrl', function ($scope, $routeParams, $q, Transaction, Account, Balance,
-        Period, Payment, PaymentMethod, PageTracking, rxSortUtil, rxPromiseNotifications,
-        DefaultPaymentMethodFilter,
-        DATE_FORMAT, TRANSACTION_TYPES, TRANSACTION_STATUSES, STATUS_MESSAGES) {
+    .controller('OverviewCtrl', function ($scope, $routeParams, $q, $timeout,
+        Balance, Payment, PaymentMethod, ContractEntity, SupportInfo,
+        Contact, Account, SupportAccount, SupportRoles,
+        DefaultPaymentMethodFilter, PrimaryAddressFilter, RoleNameFilter,
+        AccountNumberUtil, rxPromiseNotifications,
+        DATE_FORMAT, STATUS_MESSAGES) {
 
-        // Action for clearing the filters
-        var resetPager = function () {
-                $scope.pager.pageNumber = 0;
-            },
-            clearFilter = function () {
-                $scope.transactionFilter = {};
-                resetPager();
-            },
-            // Action for setting the sort
-            sortCol = function (predicate) {
-                return rxSortUtil.sortCol($scope, predicate);
-            },
-            setPaymentInfo = function (result) {
+        var RAN = AccountNumberUtil.getRAN($routeParams.accountNumber),
+            RCN = AccountNumberUtil.getRCN($routeParams.accountNumber),
+            accountType = AccountNumberUtil.getAccountType($routeParams.accountNumber);
+
+        var setPaymentInfo = function (result) {
                 // Get Current Due from Account Information, first promise of $q.all
                 $scope.paymentAmount = result[0].amountDue;
 
@@ -57,7 +52,7 @@ angular.module('billingApp')
             },
             postPayment = function (amount, methodId) {
                 $scope.paymentResult = Payment.post({
-                    id: $routeParams.accountNumber,
+                    id: RAN,
                     payment: {
                         amount: amount,
                         methodId: methodId
@@ -68,36 +63,41 @@ angular.module('billingApp')
                     success: STATUS_MESSAGES.payment.success,
                     error: STATUS_MESSAGES.payment.error
                 }, 'makePayment');
+            },
+            billingContacts = function (contacts) {
+                var billingContact = _.first(contacts);
+                if (!_.isEmpty(billingContact)) {
+                    $scope.contactAddress = PrimaryAddressFilter(billingContact.addresses.address);
+                    $scope.contactName = [billingContact.firstName, billingContact.lastName].join(' ');
+                }
+            },
+            accountManager = function (roles) {
+                var accountManager = _.first(RoleNameFilter(roles, 'Account Manager'));
+                if (!_.isEmpty(accountManager)) {
+                    $scope.accountManager = accountManager.user;
+                }
             };
-
-        // Create an instance of the PageTracking component
-        $scope.pager = PageTracking.createInstance();
-        $scope.resetPager = resetPager;
-
-        // Set the default sort of the transactions
-        $scope.sort = rxSortUtil.getDefault('date', true);
-        $scope.sortCol = sortCol;
 
         // Default Date Format
         $scope.defaultDateFormat = DATE_FORMAT;
 
         // Assign template actions
         $scope.postPayment = postPayment;
-        $scope.clearFilter = clearFilter;
 
-        // Get Account & Transactions Info
-        $scope.account = Account.get({ id: $routeParams.accountNumber });
-        $scope.balance = Balance.get({ id: $routeParams.accountNumber });
-        $scope.transactions = Transaction.list({ id: $routeParams.accountNumber });
-        $scope.paymentMethods = PaymentMethod.list({ id: $routeParams.accountNumber });
-        $scope.billingPeriods = Period.list({ id: $routeParams.accountNumber });
+        // Get Account & Contact Info
+        $scope.account = Account.get({ id: RCN, type: accountType });
+        $scope.contacts = Contact.list({ id: RCN, type: accountType, role: 'BILLING' }, billingContacts);
+        $scope.supportAccount = SupportAccount.get({ id: RCN });
+        $scope.supportRoles = SupportRoles.list({ id: RCN }, accountManager);
 
+        $scope.balance = Balance.get({ id: RAN });
+        $scope.contractEntity = ContractEntity.get({ id: RAN });
+        $scope.supportInfo = SupportInfo.get({ id: RAN });
+
+        $scope.paymentMethods = PaymentMethod.list({ id: RAN });
         // Group the promises in $q.all for a global error message if any errors occur
         rxPromiseNotifications.add($q.all([
-            $scope.balance.$promise,
-            $scope.transactions.$promise,
-            $scope.paymentMethods.$promise,
-            $scope.billingPeriods.$promise
+            $scope.balance.$promise
         ]), {
             loading: '',
             error: STATUS_MESSAGES.overview.error + ' Message: "{{_.first(data).message}}"'
@@ -105,16 +105,5 @@ angular.module('billingApp')
 
         // Set defaults for the make a payment modal.
         $q.all([$scope.balance.$promise, $scope.paymentMethods.$promise]).then(setPaymentInfo);
-
-        // Transaction Filter for the list of transactions
-        $scope.transactionFilter = {};
-
-        // Replace with service layer calls
-        // This is most likely done differently, from an API call maybe? similar concept though.
-        $scope.filterData = {
-            types: TRANSACTION_TYPES,
-            statuses: TRANSACTION_STATUSES,
-            periods: $scope.billingPeriods
-        };
 
     });
