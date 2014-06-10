2f8c6704683b64bd3806df8ac8e68e6e03d64557
Freddy Knuth <freddy.knuth@rackspace.com>
2014-06-10 15:28:48 -0500

    support account mock data

---
+++ test/api-mocks/responses/support/support-account.js
@@ -0,0 +1,29 @@
+{
+    "service_level": null,
+    "_links": {
+        "self": {
+            "href": "https://staging.dfw.support.encore.rackspace.com/api/support-accounts/473500",
+            "id": "473500"
+        },
+        "badges": {
+            "href": "https://staging.dfw.support.encore.rackspace.com/api/support-accounts/473500/badges"
+        },
+        "roles": {
+            "href": "https://staging.dfw.support.encore.rackspace.com/api/support-accounts/473500/roles"
+        },
+        "teams": {
+            "href": "https://staging.dfw.support.encore.rackspace.com/api/support-accounts/473500/teams"
+        }
+    },
+    "name": null,
+    "tags": [],
+    "linked_account_number": null,
+    "support_region": "US",
+    "type": "CLOUD",
+    "number": "473500",
+    "instructions": {
+        "date": "2014-06-10T17:34:00Z",
+        "text": "This is a test.\r\n\r\nAdded space\r\n\r\nAnother space",
+        "author_sso": "stephen.duerr"
+    }
+}
