fd84193c346208e87992bd47bff911bf758e5e2b
Josh Miller <notjoshmiller@gmail.com>
2014-08-01 15:53:41 -0500

    Implement rxModalUtil.

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -82,4 +82,145 @@ angular.module('rxGenericUtil', [])
             }
             return loadingMsg;
         };
+    })
+/**
+ *
+ * @ngdoc service
+ * @name encore.ui.rxModalUtil
+ * @description
+ * Common functionality when defining a controller for a modal
+ * var modal = rxModalUtil.getModal($scope, defaultStackName, MESSAGES.downgrade, SupportResponseError);
+ *
+ * var downgradeAccount = function () {
+ *     $scope.downgradeResult = SupportAccount.downgradeService($routeParams.accountNumber,
+ *                                                              $scope.fields.ticketNumber);
+ *     $scope.downgradeResult.$promise.then(modal.successClose('page'), modal.fail());
+ *
+ *     modal.processing($scope.downgradeResult.$promise);
+ * };
+ *
+ * modal.clear();
+ * $scope.submit = downgradeAccount;
+ * $scope.cancel = $scope.$dismiss;
+ */
+    .factory('rxModalUtil', function (rxNotify) {
+        var stacks = ['page'];
+        var util = {};
+
+        // Register stacks to be used when clearing them
+        util.registerStacks = function () {
+            stacks = stacks.concat(_.toArray(arguments));
+            return stacks;
+        };
+
+        // Clear notifications that have been used by the controller
+        util.clearNotifications = function () {
+            _.each(stacks, rxNotify.clear, rxNotify);
+            return stacks;
+        };
+
+        // Promise resolved callback for adding a notification
+        util.success = function (message, stack) {
+            return function () {
+                return rxNotify.add(message, {
+                    stack: stack,
+                    type: 'success'
+                });
+            };
+        };
+
+        // Promise resolved callback for adding a notification and closing the modal
+        util.successClose = function (scope, message, stack) {
+            var success = util.success(message, stack);
+            return function (data) {
+                scope.$close(data);
+                return success(data);
+            };
+        };
+
+        // Promise reject callback for adding a notification
+        util.fail = function (errorParser, message, stack) {
+            return function (data) {
+                if (_.isFunction(errorParser)) {
+                    var responseMsg = errorParser(data);
+
+                    if (!_.isEmpty(responseMsg)) {
+                        message += ': (' + responseMsg.msg + ')';
+                    }
+                }
+
+                return rxNotify.add(message, {
+                    stack: stack,
+                    type: 'error'
+                });
+            };
+        };
+
+        // Promise reject callback for adding a notification and dismissing the modal
+        util.failDismiss = function (scope, errorParser, message, stack) {
+            var fail = util.fail(errorParser, message, stack);
+            return function (data) {
+                scope.$dismiss(data);
+                return fail(data);
+            };
+        };
+
+        // Promise loading notification & postHook
+        util.processing = function (scope, message, defaultStack) {
+            return function (promise, stack) {
+                util.clearNotifications();
+
+                // Capture the instance of the loading notification in order to dismiss it once done processing
+                var loading = rxNotify.add(message, {
+                    stack: stack || defaultStack,
+                    loading: true
+                });
+
+                promise.finally(function () {
+                    rxNotify.dismiss(loading);
+                });
+
+                // Call the postHook (if) defined on succesful resolution
+                if (scope.postHook) {
+                    promise.then(scope.postHook);
+                }
+
+                return promise;
+            };
+        };
+
+        // Create a modal controller instance for behavior of modal within a modal controller
+        util.getModal = function (scope, defaultStack, messages, errorParser) {
+            if (!(this instanceof util.getModal)) {
+                return new util.getModal(scope, defaultStack, messages, errorParser);
+            }
+            messages = _.defaults(messages, {
+                success: null,
+                error: null,
+                loading: null
+            });
+            util.registerStacks(defaultStack);
+            this.processing = util.processing(scope, messages.loading, defaultStack);
+
+            this.getStack = function (stack) {
+                return stack || defaultStack;
+            };
+            this.success = function (stack) {
+                return util.success(messages.success, this.getStack(stack));
+            };
+            this.successClose = function (stack) {
+                return util.successClose(scope, messages.success, this.getStack(stack));
+            };
+            this.fail = function (stack) {
+                return util.fail(errorParser, messages.error, this.getStack(stack));
+            };
+            this.failDismiss = function (stack) {
+                return util.failDismiss(scope, errorParser, messages.error, this.getStack(stack));
+            };
+            this.clear = function () {
+                return util.clearNotifications();
+            };
+        };
+
+        return util;
     });
