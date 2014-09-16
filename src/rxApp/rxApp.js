d94c17a599b7e5b4e62ab010a49a53bb1ede46ff
Kevin Lamping <kevin.lamping@rackspace.com>
2014-09-16 14:57:56 -0500

    fix(rxApp): changed encoreRoutes to be a factory, eh

--- src/rxApp/rxApp.js
+++ src/rxApp/rxApp.js
@@ -1,7 +1,7 @@
 angular.module('encore.ui.rxApp', ['encore.ui.rxAppRoutes', 'encore.ui.rxEnvironment', 'ngSanitize',
     'ngRoute', 'cfp.hotkeys'])
 // This service creates an instance of AppRoutes that is used for the Encore App nav
-.service('encoreRoutes', function (rxAppRoutes, routesCdnPath, rxNotify, $q, $http) {
+.factory('encoreRoutes', function (rxAppRoutes, routesCdnPath, rxNotify, $q, $http) {
     var encoreRoutes = new rxAppRoutes();
 
     var setFailureMessage = function () {
