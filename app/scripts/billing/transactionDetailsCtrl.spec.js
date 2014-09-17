dd71744618a8783c9c2a5457cc974d72006837f2
Josh Miller <notjoshmiller@gmail.com>
2014-09-17 11:54:53 -0500

    feat(invoices): Add styles for invoice details page. Make action red less painful.
    feat(invoices): Implement invoice details template. Hide invoice method in payment modal.
    
    feat(invoices): Add save-as library via bower for PDF downloading.
    
    feat(invoices): Add download link functionality for PDF and CSV from BSL.
    
    fix(invoices): Change hyphens to underscores in PDF/CSV filenames for better readability.
    
    test(invoices): Unit tests for PDF and CSV downloads
    
    feat(transactions): Add horizontal rule to all transaction details pages for consistency
    
    fix(invoices): Address issues from PR #83
    
    fix(styles): Remove stupid space
    
    fix(invoices): Proper HTML formatting

--- app/scripts/billing/transactionDetailsCtrl.spec.js
+++ app/scripts/billing/transactionDetailsCtrl.spec.js
@@ -1,12 +1,12 @@
 describe('Billing: TransactionDetailsCtrl', function () {
-    var scope, ctrl, account, transaction;
+    var scope, ctrl, account, transaction, saveAs, httpBackend, transactionMock, notify;
 
     var testAccountNumber = '12345';
 
     beforeEach(function () {
         module('billingApp');
 
-        inject(function ($controller, $rootScope, Account, Transaction, PageTracking, $q) {
+        inject(function ($controller, $rootScope, $httpBackend, Account, Transaction, PageTracking, $q) {
             var getResourceResultMock = function (data) {
                     var deferred = $q.defer();
                     data.$promise = deferred.promise;
@@ -23,39 +23,89 @@ describe('Billing: TransactionDetailsCtrl', function () {
             scope = $rootScope.$new();
             transaction = Transaction;
             account = Account;
+            httpBackend = $httpBackend;
+            notify = { add: sinon.stub() };
 
             account.get = sinon.stub(account, 'get', getResourceMock({}));
-            transaction.getDetails = sinon.stub(transaction, 'getDetails', getResourceMock({}));
+            transactionMock = {
+                invoice: {
+                    tranRefNum: 'B1-44'
+                }
+            };
+            transaction.getDetails = sinon.stub(transaction, 'getDetails', getResourceMock(transactionMock));
+
+            saveAs = sinon.stub(window, 'saveAs');
 
             ctrl = $controller('TransactionDetailsCtrl',{
                 $scope: scope,
                 Transaction: transaction,
                 Account: account,
-                $routeParams: { accountNumber: testAccountNumber }
+                rxNotify: notify,
+                $routeParams: {
+                    accountNumber: testAccountNumber,
+                    transactionType: 'invoice',
+                    transactionNumber: 'B1-44'
+                }
             });
         });
     });
 
-    it('TransactionDetailsCtrl should exist', function () {
+    afterEach(function () {
+        window.saveAs.restore();
+    });
+
+    it('should exist', function () {
         expect(ctrl).to.exist;
     });
 
-    it('TransactionDetailsCtrl should have a sort object defined', function () {
+    it('should have a sort object defined', function () {
         expect(scope.sort).to.be.a('object');
         expect(scope.sort).to.have.property('predicate');
         expect(scope.sort.predicate).to.eq('tranDate');
     });
 
-    it('TransactionDetailsCtrl should have a default date format', function () {
+    it('should have a default date format', function () {
         expect(scope.defaultDateFormat).to.be.eq('MM / dd / yyyy');
     });
 
-    it('TransactionDetailsCtrl should get account info', function () {
+    it('should get account info', function () {
         sinon.assert.calledOnce(transaction.getDetails);
     });
 
-    it('TransactionDetailsCtrl should return a sorting predicate when calling sortCol', function () {
+    it('should return a sorting predicate when calling sortCol', function () {
         scope.sortCol('date');
         expect(scope.sort.predicate).to.be.eq('date');
+    });
+
+    it('should download a PDF when downloadPdf is called', function () {
+        scope.downloadPdf();
+        httpBackend.expectGET('/api/billing/020-12345/balance').respond(200, {});
+        httpBackend.expectGET('/api/billing/020-12345/invoice/B1-44').respond(200, '');
+        httpBackend.flush();
+        sinon.assert.calledOnce(saveAs);
+    });
+
+    it('should display an error if there is no PDF to download', function () {
+        scope.downloadPdf();
+        httpBackend.expectGET('/api/billing/020-12345/balance').respond(200, {});
+        httpBackend.expectGET('/api/billing/020-12345/invoice/B1-44').respond(404, '');
+        httpBackend.flush();
+        expect(notify.add.args[0][0]).to.equal('Invoice PDF Not Found');
+    });
+
+    it('should download a CSV when downloadCsv is called', function () {
+        scope.downloadCsv();
+        httpBackend.expectGET('/api/billing/020-12345/balance').respond(200, {});
+        httpBackend.expectGET('/api/billing/020-12345/invoice/B1-44/detail').respond(200, '');
+        httpBackend.flush();
+        sinon.assert.calledOnce(saveAs);
+    });
+
+    it('should display an error if there is no CSV to download', function () {
+        scope.downloadCsv();
+        httpBackend.expectGET('/api/billing/020-12345/balance').respond(200, {});
+        httpBackend.expectGET('/api/billing/020-12345/invoice/B1-44/detail').respond(404, '');
+        httpBackend.flush();
+        expect(notify.add.args[0][0]).to.equal('Invoice CSV Not Found');
     });
 });
