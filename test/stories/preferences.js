f4b8e5455bf5f52eadda249ffe787979f431b732
Nando Pena <fernando.pena@rackspace.com>
2014-10-02 13:48:25 -0500

    Revert "Merge branch 'master' of github.com:rackerlabs/billing-ui"
    This reverts commit 6681810d43603a4246f083f8af683f9f90091d3a, reversing
    changes made to e92cf25c2241a62594a250b9a969c370c5b2f3a3.

--- test/stories/preferences.js
+++ test/stories/preferences.js
@@ -22,96 +22,12 @@ describe('preferences page', function () {
         expect(encore.rxPage.main.subtitle).to.eventually.equal('On account Digitas London');
     });
 
-    it('should update the paperless billing preference without saving it', function () {
-        preferencesPage.paperlessBilling.invoices.then(function (currentSetting) {
-            preferencesPage.paperlessBilling.invoices = !currentSetting;
-            expect(preferencesPage.paperlessBilling.invoices).not.to.eventually.equal(currentSetting);
-        });
-    });
-
-    it('should update the notifications preference without saving it', function () {
-        preferencesPage.notifications.successfulPayments.then(function (currentSetting) {
-            preferencesPage.notifications.successfulPayments = !currentSetting;
-            expect(preferencesPage.notifications.successfulPayments).not.to.eventually.equal(currentSetting);
-        });
-    });
-
-    it('should update the paperless billing preference and save it @staging', function () {
-        preferencesPage.paperlessBilling.invoices.then(function (currentSetting) {
-            preferencesPage.paperlessBilling.invoices = !currentSetting;
-            preferencesPage.submit();
-            expect(preferencesPage.notifications.successfulPayments).not.to.eventually.equal(currentSetting);
-        });
-    });
-
-    it('should update the notifications preference and save it @staging', function () {
-        preferencesPage.notifications.successfulPayments.then(function (currentSetting) {
-            preferencesPage.notifications.successfulPayments = !currentSetting;
-            preferencesPage.submit();
-            expect(preferencesPage.notifications.successfulPayments).not.to.eventually.equal(currentSetting);
-        });
-    });
-
-    it('should update both the paperless billing and notifications preferences and save it @staging', function () {
-        preferencesPage.paperlessBilling.invoices.then(function (paperless) {
-            preferencesPage.notifications.successfulPayments.then(function (payments) {
-                preferencesPage.paperlessBilling.invoices = !paperless;
-                preferencesPage.notifications.successfulPayments = !payments;
-                preferencesPage.submit();
-                expect(preferencesPage.paperlessBilling.invoices).not.to.eventually.equal(paperless);
-                expect(preferencesPage.notifications.successfulPayments).not.to.eventually.equal(payments);
-            });
-        });
-    });
-
-    it('should not save unless the update button is clicked', function () {
-        browser.refresh();
-        preferencesPage.paperlessBilling.invoices.then(function (paperless) {
-            preferencesPage.notifications.successfulPayments.then(function (payments) {
-                preferencesPage.paperlessBilling.invoices = !paperless;
-                preferencesPage.notifications.successfulPayments = !payments;
-                browser.refresh();
-                expect(preferencesPage.paperlessBilling.invoices).to.eventually.equal(paperless);
-                expect(preferencesPage.notifications.successfulPayments).to.eventually.equal(payments);
-            });
-        });
-    });
-
-    describe('unsuccessful updates @dev', function () {
-
-        before(function () {
-            preferencesPage.search('8675309');
-        });
-
-        afterEach(function () {
-            encore.rxNotify.all.dismiss();
-        });
-
-        it('should show an error if changing the paperless billing preference fails', function () {
-            preferencesPage.paperlessBilling.invoices.then(function (currentSetting) {
-                preferencesPage.paperlessBilling.invoices = !currentSetting;
-                preferencesPage.submit();
-                encore.rxNotify.all.byType('error').then(function (errors) {
-                    expect(errors[0].text).to.eventually.equal('Error Updating Billing Preferences');
-                });
-            });
-        });
-
-        it('should show an error if changing the notifications preference fails', function () {
-            preferencesPage.notifications.successfulPayments.then(function (currentSetting) {
-                preferencesPage.notifications.successfulPayments = !currentSetting;
-                preferencesPage.submit();
-                encore.rxNotify.all.byType('error').then(function (errors) {
-                    expect(errors[0].text).to.eventually.equal('Error Updating Payment Preferences');
-                });
-            });
-        });
-
-        after(function () {
-            preferencesPage.search(browser.params.accountId);
-        });
-
-    });
+    it('should update the paperless billing preference');
+    it('should update the notifications preference');
+    it('should update both the paperless billing and notifications preferences');
+    it('should show an error if changing the paperless billing preference fails @dev');
+    it('should show an error if changing the notifications preference fails @dev');
+    it('should not save unless the update button is selected on');
 
     after(function () {
         loginPage.logout();
