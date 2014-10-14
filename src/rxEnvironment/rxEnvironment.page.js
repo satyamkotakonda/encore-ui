2a62d3d6b394a288987eafa5c7957014823317cf
Droogans <droogans@gmail.com>
2014-10-14 17:49:55 -0500

    test(rxEnvironment): Environment-checking utilities

--- src/rxEnvironment/rxEnvironment.page.js
+++ src/rxEnvironment/rxEnvironment.page.js
@@ -6,36 +6,55 @@ exports.rxEnvironment = {
     // The default is set to something simple and reasonable,
     // but should you find a need to supply your own environments, be
     // sure to have `environments` defined in your protractor conf's params section.
-    current: (function () {
-        var environments = browser.params.environments || {
-            'localhost': 'localhost',
-            'staging': 'staging',
-            'preprod': 'preprod'
-        };
-
+    current: function () {
+        var component = this;
         return browser.getCurrentUrl().then(function (url) {
-            return _.find(environments, function findEnvironment (env) {
-                if (_.contains(url, env)) {
-                    return env;
-                }
-            }) || 'production';
+            return component.compare(url);
         });
-    })(),
-
-    isLocalhost: function () {
-        return _.isEqual(exports.rxEnvironment.current, 'localhost');
     },
 
-    isStaging: function () {
-        return _.isEqual(exports.rxEnvironment.current, 'staging');
+    // Return the original environment, as defined in the current protractor conf file.
+    // Returns a promise to keep the usage consistent with `rxEnvironment.current`.
+    original: function () {
+        return protractor.promise.fulfilled(this.compare(browser.baseUrl));
     },
 
-    isPreprod: function () {
-        return _.isEqual(exports.rxEnvironment.current, 'preprod');
+    compare: function (url) {
+        return _.find(this.environments, function findEnvironment (envName, env) {
+            if (_.contains(url, env)) {
+                return envName;
+            }
+        }) || 'production';
     },
 
-    isProd: function () {
-        return _.isEqual(exports.rxEnvironment.current, 'production');
+    // Clones the environments param before filling in any missing entries with the defaults below.
+    environments: _.defaults(_.clone(browser.params.environments) || {}, {
+        'localhost': 'localhost',
+        'staging': 'staging',
+        'preprod': 'preprod'
+    }),
+
+    isLocalhost: function (useBaseUrl) {
+        return this.confirmEnvironment(useBaseUrl, 'localhost');
+    },
+
+    isStaging: function (useBaseUrl) {
+        return this.confirmEnvironment(useBaseUrl, 'staging');
+    },
+
+    isPreprod: function (useBaseUrl) {
+        return this.confirmEnvironment(useBaseUrl, 'preprod');
+    },
+
+    isProd: function (useBaseUrl) {
+        return this.confirmEnvironment(useBaseUrl, 'production');
+    },
+
+    confirmEnvironment: function (useBaseUrl, environment) {
+        var component = this;
+        return browser.getCurrentUrl().then(function (url) {
+            return _.isEqual(component.compare(useBaseUrl ? protractor.baseUrl : url), environment);
+        });
     }
 
 };
